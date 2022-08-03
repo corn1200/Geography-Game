de77227be632acbc94ae4db3a94b1b6e99bb47fc
corn1200 <corn120033@gmail.com>
2022-08-03 13:03:33 +0900

    Create ColorSetting.cs
    행성 색상을 조정하는 스크립트를 추가한다

---
+++ Assets/Script/ColorSetting.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class ColorSetting : ScriptableObject
+{
+    public Color planetColor;
+}
