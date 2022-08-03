49434e144482ea9d79d199a744ab8521680bd658
김현승 <corn120033@gmail.com>
2022-08-04 02:53:55 +0900

    Update ShapeGenerator.cs
    노이즈 생성 기능 추가

--- Assets/Script/ShapeGenerator.cs
+++ Assets/Script/ShapeGenerator.cs
@@ -5,16 +5,42 @@ using UnityEngine;
 public class ShapeGenerator : MonoBehaviour
 {
     ShapeSettings settings;
+    NoiseFilter[] noiseFilter;
 
     // 생성자
     public ShapeGenerator(ShapeSettings settings)
     {
         this.settings = settings;
+        noiseFilter = new NoiseFilter[settings.noiseLayers.Length];
+        for (int i = 0; i < noiseFilter.Length; i++)
+        {
+            noiseFilter[i] = new NoiseFilter(settings.noiseLayers[i].noiseSettings);
+        }
     }
 
     // 지정한 크기 값만큼 Planet 크기 키우기
     public Vector3 CalculatePointOnPlanet(Vector3 pointOnUnitSphere)
     {
-        return pointOnUnitSphere * settings.planetRadius;
+        float firstLayerValue = 0;
+        float elevation = 0;
+
+        if (noiseFilter.Length > 0)
+        {
+            firstLayerValue = noiseFilter[0].Evaluate(pointOnUnitSphere);
+            if (settings.noiseLayers[0].enabled)
+            {
+                elevation = firstLayerValue;
+            }
+        }
+
+        for (int i = 0; i < noiseFilter.Length; i++)
+        {
+            if (settings.noiseLayers[i].enabled)
+            {
+                float mask = (settings.noiseLayers[i].useFirstLayerAsMask) ? firstLayerValue : 1;
+                elevation += noiseFilter[i].Evaluate(pointOnUnitSphere);
+            }
+        }
+        return pointOnUnitSphere * settings.planetRadius * (1 + elevation);
     }
 }
