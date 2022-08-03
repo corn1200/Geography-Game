94937f72626826b5caa8fde0cf5e387e3cf0e65f
corn1200 <corn120033@gmail.com>
2022-08-03 12:04:31 +0900

    Update TerrainFace.cs
    주석 추가

--- Assets/Script/TerrainFace.cs
+++ Assets/Script/TerrainFace.cs
@@ -23,6 +23,7 @@ public class TerrainFace
         axisB = Vector3.Cross(localUp, axisA);
     }
 
+    // 메쉬 지정
     public void ConstructMesh()
     {
         // 총 정점 개수만큼 벡터 배열 생성
@@ -38,13 +39,17 @@ public class TerrainFace
             {
                 // 각 정점을 차례로 순회하고 다음 행 순서로 넘어가도록 함
                 int i = x + y * resolution;
+                // 각 점의 위치를 계산하고 배열에 삽입
                 Vector2 percent = new Vector2(x, y) / (resolution - 1);
                 Vector3 pointOnUnitCube = localUp + (percent.x - .5f) * 2 * axisA + (percent.y - .5f) * 2 * axisB;
+                // 벡터값을 정규화하여 점들이 원을 이루도록함
                 Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
                 vertices[i] = pointOnUnitSphere;
 
+                // 마지막 좌표 외 전부 실행
                 if (x != resolution - 1 && y != resolution - 1)
                 {
+                    // 삼각형들의 위치, 순서 지정
                     triangles[triangleIndex] = i;
                     triangles[triangleIndex + 1] = i + resolution + 1;
                     triangles[triangleIndex + 2] = i + resolution;
@@ -56,9 +61,11 @@ public class TerrainFace
                 }
             }
         }
+        // 기존 메쉬 내용을 지우고 정점과 삼각형을 초기화해준다
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        // 메쉬끼리 이어준다
         mesh.RecalculateNormals();
     }
 }
