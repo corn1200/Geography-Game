7c9cb62ede6719c5f635533fe3c3be0e17c04fd6
김현승 <corn120033@gmail.com>
2022-08-04 02:43:14 +0900

    Update ShapeSettings.cs
    노이즈 레이어 클래스와 노이즈 레이어 배열 필드를 생성한다

--- Assets/Script/ShapeSettings.cs
+++ Assets/Script/ShapeSettings.cs
@@ -7,4 +7,13 @@ public class ShapeSettings : ScriptableObject
 {
     // 행성 크기 값
     public float planetRadius = 1;
+    public NoiseLayer[] noiseLayers;
+
+    [System.Serializable]
+    public class NoiseLayer
+    {
+        public bool enabled = true;
+        public bool useFirstLayerAsMask;
+        public NoiseSettings noiseSettings;
+    }
 }
